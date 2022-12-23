493fce17b08f992e5ddeb01cef30ac17b6877c37
Mateus Jesus <mateusjdev@gmail.com>
2022-12-22 21:00:39 -0300

    fix: conexão do mongoose

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import path from 'node:path';
+import http from 'node:http';
 
 import { router } from './router';
 
-mongoose.connect("mongodb://localhost:27017")
+const app = express();
+const server = http.createServer(app);
+
+mongoose.connect('mongodb://localhost:27017')
 	.then(() => {
-		const app = express();
 		const port = 3001;
 
+		app.use((req, res, next) => {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', '*'); // * = WILDCARD
+			res.setHeader('Access-Control-Allow-Headers', '*');
+
+			next();
+		});
 		/*
 			Dizendo que os arquivos que estão dentro da pasta uploads são arquivos
 			estáticos, passando o caminho relativo para o arquivo
@@ -17,7 +27,7 @@ mongoose.connect("mongodb://localhost:27017")
 		app.use(express.json()); // transforma em um objeto javascript
 		app.use(router);
 
-		app.listen(port, () => {
+		server.listen(port, () => {
 			console.log(`🚀 Server is running on http://localhost:${port}`);
 		});
 	})
