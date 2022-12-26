d31b4a62f7c20681967d57cbddc8788213e7b008
Mateus Jesus <mateusjdev@gmail.com>
2022-12-26 14:46:49 -0300

    feat: criação e estilização do menu

--- app/src/components/Menu/styles.ts
+++ app/src/components/Menu/styles.ts
@@ -1,3 +1,24 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Product = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProductImage = styled.Image`
+  width: 120px;
+  height: 96px;
+  border-radius: 8px;
+`;
+
+export const ProductDetails = styled.View`
+  margin-left: 16px;
+  flex: 1; // ocupa todo o espaço disponível
+`;
+
+export const Separator = styled.View`
+  width: 100%;
+  height: 1px;
+  margin: 24px 0;
+  background: rgba(204, 204, 204, 0.3);
+`;
