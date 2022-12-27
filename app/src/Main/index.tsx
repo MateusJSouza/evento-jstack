08c7f7b7e948b6cc5325cb17c7c58e8f62e9a458
Mateus Jesus <mateusjdev@gmail.com>
2022-12-27 17:39:07 -0300

    feat: estilização da lista de pedidos no carrinho

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import { Button } from '../components/Button';
+import { Cart } from '../components/Cart';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
 import { TableModal } from '../components/TableModal';
+import { products } from '../mocks/products';
+import { CartItem } from '../types/CartItem';
 import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from './styles';
 
 export function Main() {
   const [isTableModalVisibile, setIsTableModalVisibile] = useState(false);
   const [selectedTable, setSelectedTable] = useState('');
+  const [cartItems, setCartItems] = useState<CartItem[]>([
+    {
+      quantity: 1,
+      product: products[0]
+    },
+    {
+      quantity: 2,
+      product: products[1],
+    }
+  ]);
 
   function handleSaveTable(table: string) {
     setSelectedTable(table);
@@ -40,6 +53,10 @@ export function Main() {
         <FooterContainer>
           {!selectedTable && (
             <Button onPress={() => setIsTableModalVisibile(true)}>Novo Pedido</Button>
+          )}
+
+          {selectedTable && (
+            <Cart cartItems={cartItems} />
           )}
         </FooterContainer>
       </Footer>
