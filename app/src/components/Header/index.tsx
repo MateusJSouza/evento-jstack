95a8c4a2bc924f79df7f5b8083786cbb48ad9b88
Mateus Jesus <mateusjdev@gmail.com>
2022-12-27 15:50:47 -0300

    feat: criação e estilização do header do pedido

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,14 +1,38 @@
+import { TouchableOpacity } from 'react-native';
 import { Text } from '../Text';
-import { Container } from './styles';
+import { Container, OrderContent, OrderHeader, TableCard } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  selectedTable: string;
+}
+
+export function Header({ selectedTable }: HeaderProps) {
   return (
     <Container>
-      <Text size={14} opacity={0.9}>Bem-vindo(a)</Text>
-      <Text size={24} weight="700">
-        WAITER
-        <Text size={24}>APP</Text>
-      </Text>
+      {!selectedTable && (
+        <>
+          <Text size={14} opacity={0.9}>Bem-vindo(a)</Text>
+          <Text size={24} weight="700">
+          WAITER
+            <Text size={24}>APP</Text>
+          </Text>
+        </>
+      )}
+
+      {selectedTable && (
+        <OrderContent>
+          <OrderHeader>
+            <Text size={24} weight="600">Pedido</Text>
+            <TouchableOpacity>
+              <Text size={14} color="#D73035" weight="600">cancelar pedido</Text>
+            </TouchableOpacity>
+          </OrderHeader>
+
+          <TableCard>
+            <Text color="#666">Mesa {selectedTable}</Text>
+          </TableCard>
+        </OrderContent>
+      )}
     </Container>
   );
 }
