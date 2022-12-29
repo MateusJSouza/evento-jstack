684d78114893c63c26b5314eb23b5125ab2a9275
Mateus Jesus <mateusjdev@gmail.com>
2022-12-29 12:46:57 -0300

    feat: estilização do carrinho de compras

--- app/src/components/Cart/index.tsx
+++ app/src/components/Cart/index.tsx
@@ -1,10 +1,20 @@
 import { FlatList, TouchableOpacity } from 'react-native';
 import { CartItem } from '../../types/CartItem';
 import { formatCurrency } from '../../utils/formatCurrency';
+import { Button } from '../Button';
 import { MinusCircle } from '../Icons/MinusCircle';
 import { PlusCircle } from '../Icons/PlusCircle';
 import { Text } from '../Text';
-import { Actions, Image, Item, ProductContainer, ProductDetails, QuantityContainer } from './styles';
+import {
+  Actions,
+  Image,
+  Item,
+  ProductContainer,
+  ProductDetails,
+  QuantityContainer,
+  Summary,
+  TotalContainer
+} from './styles';
 
 interface CartProps {
   cartItems: CartItem[];
@@ -12,43 +22,60 @@ interface CartProps {
 
 export function Cart({ cartItems }: CartProps ) {
   return (
-    <FlatList
-      data={cartItems}
-      keyExtractor={cartItem => cartItem.product._id}
-      renderItem={({ item: cartItem }) => (
-        <Item>
-          <ProductContainer>
-            <Image source={{
-              uri: `http://192.168.0.9:3001/uploads/${cartItem.product.imagePath}`
-            }} />
+    <>
+      <FlatList
+        data={cartItems}
+        keyExtractor={cartItem => cartItem.product._id}
+        style={{
+          marginBottom: 20,
+          maxHeight: 150
+        }}
+        renderItem={({ item: cartItem }) => (
+          <Item>
+            <ProductContainer>
+              <Image source={{
+                uri: `http://192.168.0.9:3001/uploads/${cartItem.product.imagePath}`
+              }} />
 
-            <QuantityContainer>
-              <Text size={14} color="#666">
-                {cartItem.quantity}x
-              </Text>
-            </QuantityContainer>
+              <QuantityContainer>
+                <Text size={14} color="#666">
+                  {cartItem.quantity}x
+                </Text>
+              </QuantityContainer>
 
-            <ProductDetails>
-              <Text size={14} weight="600">
-                {cartItem.product.name}
-              </Text>
+              <ProductDetails>
+                <Text size={14} weight="600">
+                  {cartItem.product.name}
+                </Text>
 
-              <Text size={14} color="#666" style={{ marginTop: 4 }}>
-                {formatCurrency(cartItem.product.price)}
-              </Text>
-            </ProductDetails>
-          </ProductContainer>
-          <Actions>
-            <TouchableOpacity style={{ marginRight: 24 }}>
-              <PlusCircle />
-            </TouchableOpacity>
+                <Text size={14} color="#666" style={{ marginTop: 4 }}>
+                  {formatCurrency(cartItem.product.price)}
+                </Text>
+              </ProductDetails>
+            </ProductContainer>
+            <Actions>
+              <TouchableOpacity style={{ marginRight: 24 }}>
+                <PlusCircle />
+              </TouchableOpacity>
 
-            <TouchableOpacity>
-              <MinusCircle />
-            </TouchableOpacity>
-          </Actions>
-        </Item>
-      )}
-    />
+              <TouchableOpacity>
+                <MinusCircle />
+              </TouchableOpacity>
+            </Actions>
+          </Item>
+        )}
+      />
+
+      <Summary>
+        <TotalContainer>
+          <Text color="#666">Total</Text>
+          <Text size={20} weight="600">{formatCurrency(120)}</Text>
+
+        </TotalContainer>
+        <Button onPress={() => alert('Confirmar pedido')}>
+          Confirmar pedido
+        </Button>
+      </Summary>
+    </>
   );
 }
