30674797a0de75bb1753069a1713f3f4cbfe7c96
Mateus Jesus <mateusjdev@gmail.com>
2022-12-29 13:26:16 -0300

    feat: lógica pra adicionar itens no carrinho

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -34,7 +34,37 @@ export function Main() {
   }
 
   function handleAddToCart(product: Product) {
-    alert(product.name);
+    if (!selectedTable) {
+      setIsTableModalVisibile(true);
+    }
+
+    setCartItems((prevState) => {
+      // Verificando se o item selecionado já está dentro do carrinho ou não
+      const itemIndex = prevState.findIndex(
+        cartItem => cartItem.product._id === product._id
+      );
+
+      // Se retornar 0 significa que encontrou
+      // Se retornar -1, quer dizer que ele não encontrou dentro da lista
+      if (itemIndex < 0) {
+        return prevState.concat({
+          quantity: 1,
+          product,
+        });
+      }
+
+      const newCartItems = [...prevState];
+
+      const item = newCartItems[itemIndex];
+
+      // Mantém todas as informações substituindo apenas o quantity, dizendo que o quantity é a quantidade que já tem + 1
+      newCartItems[itemIndex] = {
+        ...item,
+        quantity: item.quantity + 1,
+      };
+
+      return newCartItems;
+    });
   }
 
   return (
